8ff4a5d19c089a2b3a9140726c96ba916a51bfc4
Jae-Domain <36468840+Jae-Domain@users.noreply.github.com>
2021-05-28 14:19:27 -0700

    Added Lineart Functionality

---
+++ removeColor/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿
+namespace removeColor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picCanvas = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // picCanvas
+            // 
+            this.picCanvas.Location = new System.Drawing.Point(44, 41);
+            this.picCanvas.Name = "picCanvas";
+            this.picCanvas.Size = new System.Drawing.Size(485, 579);
+            this.picCanvas.TabIndex = 0;
+            this.picCanvas.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SeaShell;
+            this.button1.Location = new System.Drawing.Point(38, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Line Art";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightCoral;
+            this.button2.Location = new System.Drawing.Point(38, 101);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 40);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Saturate Reds";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button3.Location = new System.Drawing.Point(38, 147);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 40);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Saturate Blues";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.LightGreen;
+            this.button4.Location = new System.Drawing.Point(38, 193);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(97, 40);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Saturate Greens";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.PapayaWhip;
+            this.groupBox1.Controls.Add(this.progressBar1);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(8, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(158, 253);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Color Modes";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Coral;
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.Controls.Add(this.button7);
+            this.groupBox2.Controls.Add(this.button8);
+            this.groupBox2.Location = new System.Drawing.Point(181, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(526, 627);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Color Modes";
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.LightGreen;
+            this.button5.Location = new System.Drawing.Point(38, 180);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(97, 40);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Saturate Greens";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.SeaShell;
+            this.button6.Location = new System.Drawing.Point(38, 42);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(97, 40);
+            this.button6.TabIndex = 1;
+            this.button6.Text = "Line Art";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button7.Location = new System.Drawing.Point(38, 134);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(97, 40);
+            this.button7.TabIndex = 3;
+            this.button7.Text = "Saturate Blues";
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.LightCoral;
+            this.button8.Location = new System.Drawing.Point(38, 88);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(97, 40);
+            this.button8.TabIndex = 2;
+            this.button8.Text = "Saturate Reds";
+            this.button8.UseVisualStyleBackColor = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.PapayaWhip;
+            this.groupBox3.Controls.Add(this.picCanvas);
+            this.groupBox3.Location = new System.Drawing.Point(227, 13);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(570, 644);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Preview";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(38, 19);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(97, 23);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(871, 683);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Anti-Colorize!";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picCanvas;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ removeColor/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace removeColor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {           
+        }
+
+        private Color isDarker(Color a, Color b)
+        {
+           if (a.GetBrightness() > b.GetBrightness())
+            {
+                return b;
+            }
+            return a;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //open file
+            //file upload code cited from https://stackoverflow.com/questions/12968138/how-to-upload-a-file-in-window-forms
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Images | *.jpg"; // file types, that will be allowed to upload
+            dialog.Multiselect = false; // allow/deny user to upload more than one file at a time
+            if (dialog.ShowDialog() == DialogResult.OK) // if user clicked OK
+            {
+                Bitmap pic = new Bitmap(dialog.FileName);
+                Color dark = pic.GetPixel(0, 0);
+
+                progressBar1.Value = 0;
+                progressBar1.Maximum = pic.Width * pic.Height;
+
+                //loop through every pixel, identify darkest color
+                for (int x = 0; x < pic.Width; x++)
+                {
+                    for (int y = 0; y < pic.Height; y++)
+                    {
+                        dark = isDarker(pic.GetPixel(x, y), dark); //set dark to be darkest pixel
+                    }
+                }
+
+                //eliminate all brighter pixels
+                for (int x = 0; x < pic.Width; x++)
+                {
+                    for (int y = 0; y < pic.Height; y++)
+                    {
+                        progressBar1.Value++; //increment progress bar
+
+                        if (pic.GetPixel(x, y).GetBrightness() > dark.GetBrightness() + .25)
+                        {
+                            pic.SetPixel(x, y, Color.White);
+                        }
+                    }
+                }
+
+                //save new image/ preview image
+                Bitmap newPic = new Bitmap(pic);
+                picCanvas.Image = newPic;
+                picCanvas.SizeMode = PictureBoxSizeMode.StretchImage;
+                picCanvas.Update();
+
+                newPic.Save("lineArt.jpg", ImageFormat.Jpeg);
+            }
+
+        }
+    }
+}
